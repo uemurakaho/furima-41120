b019c0cd69ece57f67f0f59126f3abce1b9dc0c9
uemurakaho <165769994+uemurakaho@users.noreply.github.com>
2024-07-01 17:53:24 +0900

    コメントアウト削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
   devise_for :users
-  # get 'items/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "items#index"
 end

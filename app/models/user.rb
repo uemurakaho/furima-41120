7570e78b6d482a5a8b85daaaaa8b536b5d875582
uemurakaho <165769994+uemurakaho@users.noreply.github.com>
2024-07-01 17:53:44 +0900

    バリデーション作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,26 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,           presence: true
-  validates :last_name,          presence: true
-  validates :first_name,         presence: true
-  validates :last_name_kana,     presence: true
-  validates :first_name_kana,    presence: true
+  validates :password, format: {
+    with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i,
+    message: "is invalid. Include both letters and numbers"
+  }
+  validates :last_name, presence: true, format: {
+    with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/,
+    message: "is invalid. Input full-width characters"
+  }
+  validates :first_name, presence: true, format: {
+    with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/,
+    message: "is invalid. Input full-width characters"
+  }
+  validates :last_name_kana, presence: true, format: {
+    with: /\A[ァ-ヶー]+\z/,
+    message: "is invalid. Input full-width katakana characters"
+  }
+  validates :first_name_kana, presence: true, format:{
+    with: /\A[ァ-ヶー]+\z/,
+    message: "is invalid. Input full-width katakana characters"
+  }
   validates :birth_date,         presence: true
 
 

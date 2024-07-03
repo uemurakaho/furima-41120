c52bc927c9746c00e4fe83bf06cef8ee966effb8
uemurakaho <165769994+uemurakaho@users.noreply.github.com>
2024-07-03 16:38:50 +0900

    ActiveHashモジュールの作成・アソシエーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category 
+  belongs_to :status
+  belongs_to :fee_status
+  belongs_to :prefecture
+  belongs_to :days_to_ship
 end

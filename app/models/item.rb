65709d71658b3e4ef38bb708b84475cbe9e74dc3
uemurakaho <165769994+uemurakaho@users.noreply.github.com>
2024-07-03 16:55:04 +0900

    itemモデルのバリデーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,21 @@ class Item < ApplicationRecord
   belongs_to :fee_status
   belongs_to :prefecture
   belongs_to :days_to_ship
+
+  validates :name,            presence: true
+  validates :content,         presence: true
+  validates :price,           presence: true
+  validates :user,            presence: true
+
+
+
+#   | name               | string | null: false |
+# | content               | text | null: false |
+# | category_id               | integer | null: false |
+# | status_id               | integer | null: false |
+# | fee_status_id               | integer | null: false |
+# | prefecture_id               | integer | null: false |
+# | days_to_ship_id               | integer | null: false |
+# | price               | integer | null: false |
+# | user              | references | null: false, foreign_key: true |
 end

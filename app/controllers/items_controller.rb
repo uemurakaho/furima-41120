41de1e63cb8373fe4ba4a89c284c5df1eb78dfe9
uemurakaho <165769994+uemurakaho@users.noreply.github.com>
2024-07-04 17:49:23 +0900

    createメソッドの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,18 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :content, :category_id, :status_id, :fee_status_id, :prefecture_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
+  end
+
 end

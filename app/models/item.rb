f011ab04ecd2b974182d45c60330d01b6375e737
uemurakaho <165769994+uemurakaho@users.noreply.github.com>
2024-07-04 15:05:08 +0900

    モデルのバリデーション作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,11 +9,15 @@ class Item < ApplicationRecord
 
   validates :name,            presence: true
   validates :content,         presence: true
-  validates :price,           presence: true
-  validates :category_id, numericality: {other_than: 1, message: "can't be blank" }
-  validates :status_id, numericality: {other_than: 1, message: "can't be blank" }
-  validates :fee_status_id, numericality: {other_than: 1, message: "can't be blank" }
-  validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank" }
-  validates :days_to_ship_id, numericality: {other_than: 1, message: "can't be blank" }
+  validates :price,           presence: true, format: { 
+    with: /\A[0-9]+\z/,
+    numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999},
+    message: "can't be blank"
+  }
+  validates :category_id,     presence: true, numericality: {other_than: 1, message: "can't be blank" }
+  validates :status_id,       presence: true, numericality: {other_than: 1, message: "can't be blank" }
+  validates :fee_status_id,   presence: true, numericality: {other_than: 1, message: "can't be blank" }
+  validates :prefecture_id,   presence: true, numericality: {other_than: 1, message: "can't be blank" }
+  validates :days_to_ship_id, presence: true, numericality: {other_than: 1, message: "can't be blank" }
 
 end

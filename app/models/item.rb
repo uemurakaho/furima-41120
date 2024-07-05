0c3cab9f365c7202761df2d8a2bb6b8041847f17
uemurakaho <165769994+uemurakaho@users.noreply.github.com>
2024-07-05 11:34:14 +0900

    Active Storageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
   extend ActiveHash::Associations::ActiveRecordExtensions
+  has_one_attached :image
   belongs_to :category 
   belongs_to :status
   belongs_to :fee_status
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Furima41120
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.active_storage.variant_processer = :mini_magick 
 
     # Configuration for the application, engines, and railties goes here.
     #

8f72b42710c284b86b94e154222ae2bc44502ace
uemurakaho <165769994+uemurakaho@users.noreply.github.com>
2024-07-05 15:02:34 +0900

    エラー文編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
-  belongs_to :user
   extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
   has_one_attached :image
   belongs_to :category 
   belongs_to :status
@@ -8,12 +8,13 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :days_to_ship
 
+  validates :image,           presence: true
   validates :name,            presence: true
   validates :content,         presence: true
   validates :price,           presence: true, format: { 
     with: /\A[0-9]+\z/,
     numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999},
-    message: "can't be blank"
+    message: "is not a number"
   }
   validates :category_id,     presence: true, numericality: {other_than: 1, message: "can't be blank" }
   validates :status_id,       presence: true, numericality: {other_than: 1, message: "can't be blank" }

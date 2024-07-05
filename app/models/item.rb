8d725aa6a7b2da1ca9ec3c6aa057c894b681af74
uemurakaho <165769994+uemurakaho@users.noreply.github.com>
2024-07-05 16:26:25 +0900

    priceバリデーション変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,11 +11,10 @@ class Item < ApplicationRecord
   validates :image,           presence: true
   validates :name,            presence: true
   validates :content,         presence: true
-  validates :price,           presence: true, format: { 
-    with: /\A[0-9]+\z/,
-    numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999},
-    message: "is not a number"
-  }
+  with_options presence: true, format: {with: /\A[0-9]+\z/} do
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999},
+                      presence: {message:"can't be blank"}
+  end
   validates :category_id,     presence: true, numericality: {other_than: 1, message: "can't be blank" }
   validates :status_id,       presence: true, numericality: {other_than: 1, message: "can't be blank" }
   validates :fee_status_id,   presence: true, numericality: {other_than: 1, message: "can't be blank" }

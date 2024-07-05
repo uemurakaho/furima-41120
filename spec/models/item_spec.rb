f5cce647382343497e733955a4ddacfc9cceb1f9
uemurakaho <165769994+uemurakaho@users.noreply.github.com>
2024-07-05 16:26:38 +0900

    テストコード実装

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,78 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品' do
+    context '商品出品がうまくいくとき' do
+      it "内容に問題ない場合" do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '商品出品がうまくいかないとき' do
+      it "imageが空では出品できない" do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it "nameが空では出品できない" do
+        @item.name = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it "contentが空では出品できない" do
+        @item.content = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Content can't be blank")
+      end
+      it "category_idが未選択では出品できない" do
+        @item.category_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it "status_idが未選択では出品できない" do
+        @item.status_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status can't be blank")
+      end
+      it "fee_status_idが未選択では出品できない" do
+        @item.fee_status_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Fee status can't be blank")
+      end
+      it "prefecture_idが未選択では出品できない" do
+        @item.prefecture_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it "days_to_ship_idが未選択では出品できない" do
+        @item.days_to_ship_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Days to ship can't be blank")
+      end
+      it "priceが空では出品できない" do
+        @item.price = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it "priceが300未満では出品できない" do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      it "priceが10,000,000以上では出品できない" do
+        @item.price = 10_000_000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+      it "priceが全角数字では出品できない" do
+        @item.price = "５００"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+    end
+  end
 end

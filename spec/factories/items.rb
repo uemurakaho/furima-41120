f863809382557f12f06259a072edcfe9e20bad7a
uemurakaho <165769994+uemurakaho@users.noreply.github.com>
2024-07-05 14:19:19 +0900

    ダミーデータ作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :item do
-    
+    name              {Faker::Lorem.word}
+    content           {Faker::Lorem.sentence}
+    category_id       {Category.all.sample.id}
+    status_id         {Status.all.sample.id}
+    fee_status_id     {FeeStatus.all.sample.id}
+    prefecture_id     {Prefecture.all.sample.id}
+    days_to_ship_id   {DaysToShip.all.sample.id}
+    price             {Faker::Number.between(from: 300, to: 9999999)}
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end

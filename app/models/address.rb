77cf218a11bc333d4ba502d4781a162631d928a0
uemurakaho <165769994+uemurakaho@users.noreply.github.com>
2024-07-11 16:08:51 +0900

    モデル・アソシエーション実装

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,9 @@
 class Address < ApplicationRecord
+  belongs_to :order
+
+  validates :postcode,      presence: true
+  validates :prefecture_id, presence: true
+  validates :city,          presence: true
+  validates :block,         presence: true
+  validates :phone_number,  presence: true
 end

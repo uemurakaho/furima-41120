666032115794442e039be05c74c6351bc752aa65
uemurakaho <165769994+uemurakaho@users.noreply.github.com>
2024-07-12 12:10:36 +0900

    コントローラーの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,21 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
   def index
     @item = Item.find(params[:item_id])
+    @order = Order.new(order_params)
+    @address = Address.new(address_params)
+    if @order.save && @address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
   end
 
+  private
+  def order_params
+    params.require(:order).merge(item_id: @item.id, user_id: current_user.id)
+  end
+
+  def address_params
+    params.require(:address).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(order_id: @order.id)
+  end
 end

ad82ab9fdc07628f4e034ff3fe080e50a025ed51
uemurakaho <165769994+uemurakaho@users.noreply.github.com>
2024-07-16 10:31:05 +0900

    formオブジェクトパターンのモデル実装

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,9 +2,4 @@ class Address < ApplicationRecord
   belongs_to :order
   belongs_to :prefecture
 
-  validates :postcode,      presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-  validates :prefecture_id, presence: true, numericality: {other_than: 1, message: "can't be blank" }
-  validates :city,          presence: true
-  validates :block,         presence: true
-  validates :phone_number,  presence: true, length: { in: 10..11 }, format: { with: /\A[0-9]+\z/, message: "is invalid. Input only number" }
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,3 +1,20 @@
 class OrderAddress
   include ActiveModel::Model 
+  attr_accessor :user_id, :item_id, :postcode, :prefecture_id, :city, :block, :building, :phone_number
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :postcode,     format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :city
+    validates :block
+    validates :phone_number, length: { in: 10..11 }, format: { with: /\A[0-9]+\z/, message: "is invalid. Input only number" }
+  end
+  validates :prefecture_id,  numericality: {other_than: 1, message: "can't be blank" }
+  
+  def save
+    item = Item.find(params[:item_id])
+    order = Order.create(user_id: current_user.id, item_id: item.id)
+    Address.create(postcode: postcode, prefecture_id: prefecture_id, city: city, block: block, building: building, phone_number: phone_number, order_id: order.id)
+  end
 end

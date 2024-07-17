fc4d870a68192decedfb87538f71eaa58c285211
uemurakaho <165769994+uemurakaho@users.noreply.github.com>
2024-07-17 11:18:13 +0900

    コントローラー修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -40,7 +40,6 @@ class OrdersController < ApplicationController
   end
 
   def move_to_index
-    @item = Item.find(params[:item_id])
     unless current_user.id != @item.user_id && @item.order.nil?
       redirect_to root_path
     end
